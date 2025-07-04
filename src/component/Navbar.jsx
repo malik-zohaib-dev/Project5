92675c6254535144947725d15825e5da7364c932
Malik Zohaib Altaf <zohaibmalik1777@gmail.com>
2025-07-04 05:03:13 +0500

    changes made

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from "react"
+
+import Vector from '../assets/Vector.png'
 
 const Navbar = () => {
   return (
     <div className="flex h-[60px] gap-2 text-xl font-medium my-4 rounded-lg bg-white items-center justify-center">
-      <img src="/Vector.png" alt="logo" />
+      <img src={Vector} alt="logo" />
       <h1>Firebase Contact App</h1>
     </div>
   );
--- src/component/Notfoundcontact.jsx
+++ src/component/Notfoundcontact.jsx
@@ -1,7 +1,9 @@
+import HandsContact from '../assets/Handscontact.png'
+
 const Notfoundcontact = () => {
   return (
     <div className="flex m-auto h-[80vh] gap-4 justify-center items-center">
-        <img src="/Handscontact.png" alt="" />
+        <img src={HandsContact} alt="" />
     <h3 className="text-white text-2xl font-semibold">Contact Not Found</h3>
     </div>
   )
